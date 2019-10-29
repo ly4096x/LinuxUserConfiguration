26a65c0e473b9046521e7f3844d5459bfeba268c
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2019-10-28 20:25:10 -0400

    commit

---
+++ setup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p .cache/zsh
+mkdir -p .vim/bundle
+git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+cp -r .oh_my_zsh_custom/* .oh-my-zsh/custom/
+git clone https://github.com/VundleVim/Vundle.vim .vim/bundle/Vundle.vim
+rm -rf setup.bash .git .oh_my_zsh_custom
