7290e303358fdb2dbe2e2506918474180c002a27
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2020-08-14 01:02:50 -0400

    include dockertools

--- setup.bash
+++ setup.bash
@@ -6,3 +6,5 @@ git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
 cp -r .oh_my_zsh_custom/* .oh-my-zsh/custom/
 git clone https://github.com/VundleVim/Vundle.vim .vim/bundle/Vundle.vim
 rm -rf setup.bash .git .oh_my_zsh_custom
+wget https://github.com/ly4096x/dockertools/raw/master/dockertools -O .local/bin/dockertools
+chmod +x .local/bin/dockertools
