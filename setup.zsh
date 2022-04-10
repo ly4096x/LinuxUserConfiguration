f037231c745e6e339834f19dcb0a5dd908e75af5
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2022-04-10 04:09:18 -0700

    simplify path

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 set -e
 
-REQUIRED_EXE='node yarn curl git'
-for x in $REQUIRED_EXE ; do
+REQUIRED_EXE=(node yarn curl git)
+for x in ${REQUIRED_EXE[@]} ; do
     if ! command -v $x ; then
         echo Require: $REQUIRED_EXE, and vim or neovim.
         exit
@@ -17,9 +17,9 @@ rm -rf setup.bash .git .oh_my_zsh_custom
 curl -Lo ~/.local/bin/dockertools --create-dirs https://github.com/ly4096x/dockertools/raw/master/dockertools
 chmod +x ~/.local/bin/dockertools
 
-curl -Lo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -Lo .local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.vim/autoload/
-ln -s $(realpath --relative-to=$HOME/.vim/autoload/ "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim) ~/.vim/autoload/
+ln -s ../../.local/share/nvim/site/autoload/plug.vim) ~/.vim/autoload/
 if ! command -v nvim &> /dev/null ; then
     vim +PlugUpdate +qall
 else
